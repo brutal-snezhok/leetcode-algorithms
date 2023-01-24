d29f065b7af25d1fc3beffb4fca68d8010cae0ef
DmitryTsyrulik <czyrulik97@mail.ru>
2023-01-24 09:36:25 +0300

    solved kth largest task

---
+++ src/main/java/heap_and_priority_queue/easy/KthLargest_703.java
@@ -0,0 +1,43 @@
+package heap_and_priority_queue.easy;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+class KthLargest_703 {
+
+    Queue<Integer> q;
+    int k;
+
+    // time O(nlogk)
+    // space O(k)
+    public KthLargest_703(int k, int[] nums) {
+        q = new PriorityQueue<>(k);
+        this.k = k;
+
+        for(int num : nums)
+            add(num);
+    }
+
+    // time O(mlogk), m time call add method
+    // space O(k)
+    public int add(int val) {
+        if(q.size() < k) {
+            q.add(val);
+            return q.peek();
+        }
+
+        if(q.peek() < val) {
+            q.poll();
+            q.add(val);
+        }
+
+        return q.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
