e5908a08b165bbd1b510e1a59f3529375b262a0d
DmitryTsyrulik <czyrulik97@mail.ru>
2023-01-26 16:45:48 +0300

    solved valid parentheses task

---
+++ src/main/java/stack/ValidParentheses_20.java
@@ -0,0 +1,27 @@
+package stack;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/valid-parentheses/
+public class ValidParentheses_20 {
+    public boolean isValid(String str) {
+        // time O(n)
+        // space O(n)
+
+        Stack<Character> s = new Stack<>();
+        for(char ch : str.toCharArray()) {
+            if(ch == '(')
+                s.push(')');
+            else if(ch == '{')
+                s.push('}');
+            else if(ch == '[')
+                s.push(']');
+            else {
+                if(s.isEmpty() || s.pop() != ch)
+                    return false;
+            }
+        }
+
+        return s.isEmpty();
+    }
+}
