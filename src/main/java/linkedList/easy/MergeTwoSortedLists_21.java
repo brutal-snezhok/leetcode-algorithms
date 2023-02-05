677856b8e0c4cd0d06bc1f027f29459dca632482
DmitryTsyrulik <czyrulik97@mail.ru>
2023-02-05 10:36:48 +0300

    solved MergeTwoSortedLists_21 task

---
+++ src/main/java/linkedList/easy/MergeTwoSortedLists_21.java
@@ -0,0 +1,78 @@
+package linkedList.easy;
+
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+public class MergeTwoSortedLists_21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // iterative
+        // time O(n + m)
+        // space O(1)
+
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        while (list1 != null) {
+            curr.next = list1;
+            list1 = list1.next;
+            curr = curr.next;
+        }
+
+        while (list2 != null) {
+            curr.next = list2;
+            list2 = list2.next;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        // recursive
+        // time O(n + m)
+        // space O(n + m)
+
+        if(list1 == null)
+            return list2;
+        if(list2 == null)
+            return list1;
+
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
