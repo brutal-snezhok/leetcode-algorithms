8dddbd7bf48396950c0cba385b6c740f954564b8
DmitryTsyrulik <czyrulik97@mail.ru>
2023-02-06 16:29:03 +0300

    solved IntersectionOfTwoLinkedLists_160 task

---
+++ src/main/java/linkedList/easy/IntersectionOfTwoLinkedLists_160.java
@@ -0,0 +1,20 @@
+package linkedList.easy;
+
+import linkedList.ListNode;
+
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+public class IntersectionOfTwoLinkedLists_160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // time O(headA + headB)
+        // space O(1)
+
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while(p1 != p2) {
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+
+        return p1;
+    }
+}
