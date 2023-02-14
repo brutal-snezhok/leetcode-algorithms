da2c27183cbf4617b6eb1f0e22145aba23540069
DmitryTsyrulik <czyrulik97@mail.ru>
2023-02-14 16:48:57 +0300

    solved TaskScheduler_621 task

--- src/main/java/heapAndPriorityQueue/medium/TaskScheduler_621.java
+++ src/main/java/heapAndPriorityQueue/medium/TaskScheduler_621.java
@@ -8,48 +8,54 @@ import java.util.Queue;
 // https://leetcode.com/problems/task-scheduler/
 public class TaskScheduler_621 {
 
-    public static int leastInterval(char[] tasks, int n) {
-        // time O(n*log26) = O(n)
-        // space O(n)
+    public int leastInterval(char[] tasks, int n) {
+        // time O(l * log26), l - length of arr tasks
+        // space O(26)
+
+        // use maxHeap
+        // count in arr {char -> count}
+        // add all count to maxHeap
+        // create queue to wait there idle time
+        // while maxHeap != null || q != null
+        // take val from maxHeap time++ check whether we can take val from q and add it to maxHeap
+
         if(n == 0) return tasks.length;
-        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+
         int[] arr = new int[26];
         for(char ch : tasks)
             arr[ch - 'A']++;
-        for(int num : arr) {
-            if(num > 0)
-                pq.add(num);
+
+        Queue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        Queue<Pair> q = new LinkedList<>();
+        for(int task : arr) {
+            if(task != 0)
+                maxHeap.add(task);
         }
 
         int time = 0;
-        while(!pq.isEmpty() || !q.isEmpty()) {
+        while(!maxHeap.isEmpty() || !q.isEmpty()) {
             time++;
-            if(!pq.isEmpty()) {
-                int val = pq.poll();
+            if(!maxHeap.isEmpty()) {
+                int val = maxHeap.poll();
                 val--;
                 if(val > 0)
                     q.add(new Pair(val, time + n));
             }
 
-            if(!q.isEmpty() && q.peek().value == time)
-                pq.add(q.poll().key);
+            if(!q.isEmpty() && q.peek().time <= time)
+                maxHeap.add(q.remove().val);
         }
 
         return time;
     }
 
-    static class Pair<K, V> {
-        K key;
-        V value;
+    class Pair{
+        int val;
+        int time;
 
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
+        public Pair(int val, int time) {
+            this.val = val;
+            this.time = time;
         }
-    }
-
-    public static void main(String[] args) {
-
     }
 }
