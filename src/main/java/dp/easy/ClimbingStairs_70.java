f0a1ccf15d1ea6cafa126b4ebbd98a200461bf79
DmitryTsyrulik <czyrulik97@mail.ru>
2023-03-27 18:03:19 +0300

    solved ClimbingStairs_70 task

---
+++ src/main/java/dp/easy/ClimbingStairs_70.java
@@ -0,0 +1,91 @@
+package dp.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/climbing-stairs/
+public class ClimbingStairs_70 {
+    // solution1
+    // TLE
+    public int climbStairs1(int n) {
+        // dfs
+        // time O(2^n)
+        // space O(n)
+
+        return dfs1(n, 0);
+    }
+
+    private int dfs1(int n, int sum) {
+        if(sum == n)
+            return 1;
+        else if(sum < n) {
+            int count = dfs1(n, sum + 1) + dfs1(n, sum + 2);
+            return count;
+        }
+
+        return 0;
+    }
+
+    // solution2
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public int climbStairs2(int n) {
+        // dfs with memo
+        // time O(n)
+        // space O(n)
+
+        return dfs2(n, 0);
+    }
+
+    private int dfs2(int n, int sum) {
+        if(map.containsKey(sum))
+            return map.get(sum);
+
+        if(sum == n)
+            return 1;
+        else if(sum < n) {
+            int count = dfs2(n, sum + 1) + dfs2(n, sum + 2);
+            map.put(sum ,count);
+            return count;
+        }
+
+        return 0;
+    }
+
+    // solution3
+    public int climbStairs3(int n) {
+        // dp
+        // time O(n)
+        // space O(n)
+        if(n == 1)
+            return 1;
+
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = 1;
+        for(int i = n - 2; i >= 0; i--)
+            dp[i] = dp[i + 1] + dp[i + 2];
+
+        return dp[0];
+    }
+
+    // solution4
+    public int climbStairs4(int n) {
+        // dp
+        // time O(n)
+        // space O(1)
+        if(n == 1)
+            return 1;
+
+        int one = 1;
+        int two = 1;
+
+        for(int i = n - 2; i >= 0; i--) {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+
+        return one;
+    }
+}
