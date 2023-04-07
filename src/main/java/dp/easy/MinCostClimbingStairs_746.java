3e4f2162704bf49bc18d099634772e0dea16a027
DmitryTsyrulik <czyrulik97@mail.ru>
2023-04-07 13:47:34 +0300

    solved MinCostClimbingStairs_746 task

--- src/main/java/dp/easy/MinCostClimbingStairs_746.java
+++ src/main/java/dp/easy/MinCostClimbingStairs_746.java
@@ -1,45 +1,102 @@
 package dp.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/problems/min-cost-climbing-stairs/
 public class MinCostClimbingStairs_746 {
+    // solution1
     public int minCostClimbingStairs1(int[] cost) {
+        // time O(2^n)
+        // space O(n)
+        // TLE
+
+        int n = cost.length;
+        int firstVal = backtracking1(cost, n - 1);
+        int secondVal = backtracking1(cost, n - 2);
+        return Math.min(firstVal, secondVal);
+    }
+
+    private int backtracking1(int[] cost, int n) {
+        if(n < 0)
+            return 0;
+        if(n == 0 || n == 1)
+            return cost[n];
+
+        int firstVal = backtracking1(cost, n - 1);
+        int secondVal = backtracking1(cost, n - 2);
+        return cost[n] + Math.min(firstVal, secondVal);
+    }
+
+    // solution2
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public int minCostClimbingStairs2(int[] cost) {
+        // dp top down with memo
         // time O(n)
         // space O(n)
 
-        if (cost.length == 0 || cost.length == 1)
-            return 0;
-
-        int[] dp = new int[cost.length + 1];
-        dp[0] = 0;
-        dp[1] = cost[0];
-
-        for (int i = 2; i < dp.length; i++)
-            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i - 1];
-
-        return Math.min(dp[cost.length], dp[cost.length - 1]);
+        int n = cost.length;
+        map.put(0, cost[0]);
+        map.put(1, cost[1]);
+        return Math.min(backtracking2(cost, n - 1), backtracking2(cost, n - 2));
     }
 
-    public int minCostClimbingStairs2(int[] cost) {
+    private int backtracking2(int[] cost, int n) {
+        if(n < 0)
+            return 0;
+
+        if(map.containsKey(n))
+            return map.get(n);
+
+        int firstVal = backtracking2(cost, n - 1);
+        int secondVal = backtracking2(cost, n - 2);
+
+        map.put(n - 1, firstVal);
+        map.put(n - 2, secondVal);
+
+        return cost[n] + Math.min(firstVal ,secondVal);
+    }
+
+    public int minCostClimbingStairs3(int[] cost) {
+        // bottom up approach
+        // time O(n)
+        // space O(n)
+
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i <= n; i++) {
+            int val = i == n ? 0 : cost[i];
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + val;
+        }
+
+        return dp[n];
+    }
+
+    public int minCostClimbingStairs4(int[] cost) {
+        // bottom up approach, optimized
         // time O(n)
         // space O(1)
 
-        if (cost.length == 0 || cost.length == 1)
-            return 0;
+        int n = cost.length;
+        int prev = cost[1];
+        int prevPrev = cost[0];
 
-        int step = 0;
-        int doubleStep = cost[0];
-
-        for (int i = 1; i < cost.length; i++) {
-            int temp = Math.min(step, doubleStep) + cost[i];
-            step = doubleStep;
-            doubleStep = temp;
+        for(int i = 2; i <= n; i++) {
+            int val = i == n ? 0 : cost[i];
+            int temp = Math.min(prev, prevPrev) + val;
+            prevPrev = prev;
+            prev = temp;
         }
 
-        return Math.min(step, doubleStep);
+        return prev;
     }
 
     public static void main(String[] args) {
         MinCostClimbingStairs_746 stairs = new MinCostClimbingStairs_746();
-       // stairs.minCostClimbingStairs1(new int[]{10, 15, 20});
+        stairs.minCostClimbingStairs1(new int[]{10, 15, 20});
     }
 }
