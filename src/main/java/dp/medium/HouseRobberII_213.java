cfa16c153554492ad44a96074fbf2d6b01ead2cc
DmitryTsyrulik <czyrulik97@mail.ru>
2023-04-08 19:37:26 +0300

    solved HouseRobberII_213 task

--- src/main/java/dp/medium/HouseRobberII_213.java
+++ src/main/java/dp/medium/HouseRobberII_213.java
@@ -2,6 +2,7 @@ package dp.medium;
 
 // https://leetcode.com/problems/house-robber-ii/
 public class HouseRobberII_213 {
+    // solution1
     public int rob(int[] nums) {
         // time O(n)
         // space O(1)
@@ -23,5 +24,30 @@ public class HouseRobberII_213 {
         }
 
         return rob2;
+    }
+
+    // solution2
+    public int rob2(int[] nums) {
+        // dp, bottom up with 2 variables
+        // time O(n)
+        // space O(1)
+
+        int n = nums.length;
+        if(n == 1)
+            return nums[0];
+
+        return Math.max(rob2(nums, 0, n - 2), rob2(nums, 1, n - 1));
+    }
+
+    private int rob2(int[] nums, int l, int r) {
+        int prev = nums[l];
+        int prevPrev = 0;
+        for(int i = l + 1; i <= r; i++) {
+            int temp = Math.max(prev, prevPrev + nums[i]);
+            prevPrev = prev;
+            prev = temp;
+        }
+
+        return prev;
     }
 }
