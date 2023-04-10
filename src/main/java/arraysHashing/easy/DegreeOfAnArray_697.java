eb6a4736dff3e0635794036535d126b5beb98fa4
DmitryTsyrulik <czyrulik97@mail.ru>
2023-04-10 15:00:03 +0300

    solved DegreeOfAnArray_697 task

---
+++ src/main/java/arraysHashing/easy/DegreeOfAnArray_697.java
@@ -0,0 +1,35 @@
+package arraysHashing.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/degree-of-an-array/description/
+public class DegreeOfAnArray_697 {
+    public int findShortestSubArray(int[] nums) {
+        // time O(n)
+        // space O(n)
+
+        Map<Integer, int[]> map = new HashMap<>(); // {val, [startInd, endInd, count]}
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int[] temp = map.get(nums[i]);
+                temp[1] = i;
+                temp[2] += 1;
+                map.put(nums[i], temp);
+            } else
+                map.put(nums[i], new int[]{i, i, 1});
+        }
+
+        int maxDegree = 0;
+        for(int[] arr : map.values())
+            maxDegree = Math.max(maxDegree, arr[2]);
+
+        int res = Integer.MAX_VALUE;
+        for(int[] arr : map.values()) {
+            if(arr[2] == maxDegree)
+                res = Math.min(res, arr[1] - arr[0] + 1);
+        }
+
+        return res;
+    }
+}
