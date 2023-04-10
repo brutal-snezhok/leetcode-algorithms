66376ccf0e1da642eb5495a068b8da0306bed048
DmitryTsyrulik <czyrulik97@mail.ru>
2023-04-10 21:43:25 +0300

    solved LargestNumberAtLeastTwiceOfOthers_747 task

---
+++ src/main/java/arraysHashing/easy/LargestNumberAtLeastTwiceOfOthers_747.java
@@ -0,0 +1,32 @@
+package arraysHashing.easy;
+
+// https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+public class LargestNumberAtLeastTwiceOfOthers_747 {
+    public static int dominantIndex(int[] nums) {
+        // time O(n)
+        // space O(1)
+
+        // find max
+        // check condition for every el
+
+        int max = nums[0];
+        int ind = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(max < nums[i]) {
+                max = nums[i];
+                ind = i;
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0 && i != ind && max / nums[i] < 2)
+                return -1;
+        }
+
+        return ind;
+    }
+
+    public static void main(String[] args) {
+        dominantIndex(new int[]{3,6,1,0});
+    }
+}
