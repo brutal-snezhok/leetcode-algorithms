e288084ad49d652163f61c03e39e5e91f332c981
DmitryTsyrulik <czyrulik97@mail.ru>
2023-04-10 11:56:29 +0300

    solved DistributeCandies_575 task

---
+++ src/main/java/arraysHashing/easy/DistributeCandies_575.java
@@ -0,0 +1,27 @@
+package arraysHashing.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/problems/distribute-candies/description/
+public class DistributeCandies_575 {
+    public int distributeCandies(int[] candyType) {
+        // time O(n)
+        // space O(n)
+
+        // count how many types of candies are = numOfTypes
+        // count how many candies Alice can eat canEat = n / 2
+        // if numOfTypes <= canEat -> numOfTypes
+        // if numOfTypes > canEat -> canEat
+
+        int n = candyType.length;
+        Set<Integer> candyTypes = new HashSet<>();
+        for(int candy : candyType)
+            candyTypes.add(candy);
+
+        int numOfTypes = candyTypes.size();
+        int canEat = n / 2;
+
+        return Math.min(numOfTypes, canEat);
+    }
+}
