177501ef5b42d3b8a750797a7742551d680315db
DmitryTsyrulik <czyrulik97@mail.ru>
2023-04-24 10:27:28 +0300

    solved MaximumLengthOfSubarrayWithPositiveProduct_1567 task

---
+++ src/main/java/dp/medium/MaximumLengthOfSubarrayWithPositiveProduct_1567.java
@@ -0,0 +1,33 @@
+package dp.medium;
+
+// https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+public class MaximumLengthOfSubarrayWithPositiveProduct_1567 {
+    public int getMaxLen(int[] nums) {
+        // time O(n)
+        // space O(1)
+
+        // keep track on count of neg and pos length
+
+        int res = 0;
+        int pos = 0;
+        int neg = 0;
+
+        for(int num : nums) {
+            if(num == 0) {
+                pos = 0;
+                neg = 0;
+            } else if(num > 0) {
+                pos++;
+                neg = neg == 0 ? 0 : neg + 1;
+            } else {
+                int tmp = pos;
+                pos = neg == 0 ? 0 : neg + 1;
+                neg = tmp + 1;
+            }
+
+            res = Math.max(res, pos);
+        }
+
+        return res;
+    }
+}
