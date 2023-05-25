d9965d918fd9db9548633768fa243f4971e14ee1
DmitryTsyrulik <czyrulik97@mail.ru>
2023-05-25 19:38:14 +0300

    solved PalindromePartitioning_131 task

--- src/main/java/backtracking/medium/PalindromePartitioning_131.java
+++ src/main/java/backtracking/medium/PalindromePartitioning_131.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 // https://leetcode.com/problems/palindrome-partitioning/description/
 public class PalindromePartitioning_131 {
-    public List<List<String>> partition(String s) {
+    // solution1
+    public List<List<String>> partition1(String s) {
         // time O(n * 2^n)
         // space O(n), didn't count res list
 
@@ -40,5 +41,36 @@ public class PalindromePartitioning_131 {
         }
 
         return true;
+    }
+
+    // solution2
+    public List<List<String>> partition2(String s) {
+        // backtracking + dp
+        // time O(n*2^n)
+        // space O(n), not count res list
+
+        List<List<String>> res = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        dfs(res, curr, s, 0, dp);
+
+        return res;
+    }
+
+    private void dfs(List<List<String>> res, List<String> curr, String s, int start, boolean[][] dp) {
+        if(start == s.length()) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int end = start; end < s.length(); end++) {
+            String substr = s.substring(start, end + 1);
+            if(s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                curr.add(substr);
+                dfs(res, curr, s, end + 1, dp);
+                curr.remove(curr.size() - 1);
+            }
+        }
     }
 }
