f83e2df3adeea754b4f6fc6b50496e6818711b91
DmitryTsyrulik <czyrulik97@mail.ru>
2023-06-11 11:30:59 +0300

    solved PopulatingNextRightPointersEachNode_116 task

--- src/main/java/tree/medium/PopulatingNextRightPointersEachNode_116.java
+++ src/main/java/tree/medium/PopulatingNextRightPointersEachNode_116.java
@@ -2,6 +2,7 @@ package tree.medium;
 
 // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
 public class PopulatingNextRightPointersEachNode_116 {
+    // solution1
     public Node connect(Node root) {
         // recursive
         // time O(n)
@@ -22,6 +23,8 @@ public class PopulatingNextRightPointersEachNode_116 {
         connect(root.left);
         connect(root.right);
     }
+
+    // solution2
     public Node connect2(Node root) {
         // iterative
         // time O(n)
@@ -40,6 +43,36 @@ public class PopulatingNextRightPointersEachNode_116 {
             if(curr == null) {
                 curr = nextLvl;
                 nextLvl = curr.left;
+            }
+        }
+
+        return root;
+    }
+
+    // solution3
+    public Node connect3(Node root) {
+        // time O(n)
+        // space O(1)
+
+        if(root == null)
+            return root;
+
+        Node curr = null;
+        Node nextLvl = root;
+
+        while(true) {
+            curr = nextLvl;
+            if(curr.left != null)
+                nextLvl = curr.left;
+            else
+                break;
+
+            while(curr != null) {
+                curr.left.next = curr.right;
+                if(curr.next != null)
+                    curr.right.next = curr.next.left;
+
+                curr = curr.next;
             }
         }
 
