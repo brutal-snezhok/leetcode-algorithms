57ec527b42a9958c26876e8d93e375bfdc6b9369
DmitryTsyrulik <czyrulik97@mail.ru>
2023-06-12 15:01:04 +0300

    solved ShortestBridge_934 task

--- src/main/java/graphs/medium/ShortestBridge_934.java
+++ src/main/java/graphs/medium/ShortestBridge_934.java
@@ -8,79 +8,83 @@ public class ShortestBridge_934 {
     int[][] directions = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 
     public int shortestBridge(int[][] grid) {
-        // time O(n^2)
-        // space O(n^2)
+        // time O(n*n)
+        // space O(n*n)
 
-        // go through grid and find first island
-        // change with dfs 1 to 2 on the first island
-        // add all points of second island to queue
-        // run bfs search from every point
+        /**
+         1. find with dfs first island
+         2. find with bfs shortest path
+         */
 
-        boolean isNotFoundFirst = true;
-        final int ROWS = grid.length;
-        final int COLS = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
+        int n = grid.length;
+        boolean[][] visited = new boolean[n][n];
+        Queue<int[]> q = new LinkedList<>(); // [row, col, length]
 
-        for(int i = 0; i < ROWS; i++) {
-            for(int j = 0; j < COLS; j++) {
-                if(grid[i][j] == 1 && isNotFoundFirst) {
-                    isNotFoundFirst = false;
-                    dfs(i, j, grid, ROWS, COLS);
+        boolean isFoundFirstIsland = false;
+        for(int r = 0; r < n; r++)
+            for(int c = 0; c < n; c++)
+                if(grid[r][c] == 1 && !isFoundFirstIsland) {
+                    isFoundFirstIsland = true;
+                    dfs(grid, r, c, visited, q);
                 }
 
-                if(grid[i][j] == 1 && !isNotFoundFirst)
-                    q.add(new int[]{i, j});
-            }
-        }
-
-        return bfs(grid, ROWS, COLS, q);
+        return bfs(grid, visited, q);
     }
 
-    private int bfs(int[][] grid, int ROWS, int COLS, Queue<int[]> q) {
-        int step = 0;
+    private int bfs(int[][] grid, boolean[][] visited, Queue<int[]> q) {
+        int n = grid.length;
         while(!q.isEmpty()) {
-            int n = q.size();
-            for(int i = 0; i < n; i++) {
-                int[] point = q.poll();
+            int size = q.size();
 
-                // add to q all children of curr point
+            for(int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+
+                if(grid[curr[0]][curr[1]] == 1)
+                    return curr[2] - 1;
+
                 for(int[] dir : directions) {
-                    int newR = point[0] + dir[0];
-                    int newC = point[1] + dir[1];
-                    if(newR >= 0 && newR < ROWS && newC >= 0 && newC < COLS) {
-                        if(grid[newR][newC] == 2)
-                            return step;
-                        if(grid[newR][newC] == 0) {
-                            grid[newR][newC] = 1;  // change to 1 to not add to q the same point again
-                            q.add(new int[]{newR, newC});
-                        }
+                    int newX = curr[0] + dir[0];
+                    int newY = curr[1] + dir[1];
+
+                    if(newX < n && newX >= 0 && newY < n && newY >= 0 && !visited[newX][newY]) {
+                        visited[newX][newY] = true;
+                        q.add(new int[]{newX, newY, curr[2] + 1});
                     }
                 }
-
             }
-            step++;
         }
 
         return -1;
     }
 
-    private void dfs(int r, int c, int[][] grid, int ROWS, int COLS) {
-        if(r < 0 || r >= ROWS || c < 0 || c >= COLS || grid[r][c] == 0 || grid[r][c] == 2)
+    private void dfs(int[][] grid, int r, int c, boolean[][] visited, Queue<int[]> q) {
+        int n = grid.length;
+        if(r >= n || r < 0 || c >= n || c < 0 || visited[r][c] || grid[r][c] == 0)
             return;
 
-        grid[r][c] = 2;
+        visited[r][c] = true;
+        grid[r][c] = 0;
+        q.add(new int[]{r, c, 0});
         for(int[] dir : directions)
-            dfs(r + dir[0], c + dir[1], grid, ROWS, COLS);
+            dfs(grid, r + dir[0], c + dir[1], visited, q);
     }
 
     public static void main(String[] args) {
         ShortestBridge_934 bridge = new ShortestBridge_934();
+//        bridge.shortestBridge(new int[][]{
+//                {1,1,1,1,1},
+//                {1,0,0,0,1},
+//                {1,0,1,0,1},
+//                {1,0,0,0,1},
+//                {1,1,1,1,1}
+//        });
+
         bridge.shortestBridge(new int[][]{
-                {1,1,1,1,1},
-                {1,0,0,0,1},
-                {1,0,1,0,1},
-                {1,0,0,0,1},
-                {1,1,1,1,1}
+                {0,1,0,0,0},
+                {0,1,0,1,1},
+                {0,0,0,0,1},
+                {0,0,0,0,0},
+                {0,0,0,0,0}
         });
     }
 }
