56e48a0323bf90d994eeeb5a429d1bf1f852331a
DmitryTsyrulik <czyrulik97@mail.ru>
2023-07-06 10:13:32 +0300

    solved ReplaceWords_648 task

--- src/main/java/trie/ReplaceWords_648.java
+++ src/main/java/trie/ReplaceWords_648.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 // https://leetcode.com/problems/replace-words/description/
 public class ReplaceWords_648 {
-    public String replaceWords(List<String> dictionary, String sentence) {
+    // solution1
+    public String replaceWords1(List<String> dictionary, String sentence) {
         // time O(n*k), n - number of strings, k - avg length of dictionary string
         // space O(k)
 
@@ -62,5 +63,71 @@ public class ReplaceWords_648 {
     class Node {
         Node[] children = new Node[26];
         String val;
+    }
+
+    // solution2
+    public String replaceWords2(List<String> dictionary, String sentence) {
+        // time O(dict.length * avg word l + sentence.length * avg word l)
+        // space O(dict.length * avg word l + sentence.length * avg word l) = size of trie + size of words arr
+        Trie2 trie = new Trie2();
+        for(String w : dictionary)
+            trie.insert(w);
+
+        String[] words = sentence.split(" ");
+        StringBuilder res = new StringBuilder();
+        for(String word : words) {
+            String root = trie.searchRoot(word);
+            if(root == null)
+                res.append(word);
+            else
+                res.append(root);
+            res.append(" ");
+        }
+
+        res.setLength(res.length() - 1); // remove last space
+        return res.toString();
+    }
+
+    class Trie2 {
+
+        Node root;
+
+        public Trie2() {
+            root = new Node();
+        }
+
+        public void insert(String word) {
+            Node curr = root;
+            for(char ch : word.toCharArray()) {
+                if(curr.children[ch - 'a'] == null)
+                    curr.children[ch - 'a'] = new Node();
+
+                curr = curr.children[ch - 'a'];
+            }
+
+            curr.isRoot = true;
+        }
+
+        public String searchRoot(String word) {
+            Node curr = root;
+            for(int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+
+                if(curr.isRoot)
+                    return word.substring(0, i);
+
+                if(curr.children[ch - 'a'] == null)
+                    return null;
+
+                curr = curr.children[ch - 'a'];
+            }
+
+            return word;
+        }
+
+        class Node {
+            Node[] children = new Node[26];
+            boolean isRoot;
+        }
     }
 }
