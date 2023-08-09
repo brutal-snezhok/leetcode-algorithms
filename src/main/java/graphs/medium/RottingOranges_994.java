934c78ad5975d06a389f6082a076802cb1abb9f5
DmitryTsyrulik <czyrulik97@mail.ru>
2023-08-09 18:22:30 +0300

    solved RottingOranges_994 task

--- src/main/java/graphs/medium/RottingOranges_994.java
+++ src/main/java/graphs/medium/RottingOranges_994.java
@@ -5,9 +5,10 @@ import java.util.Queue;
 
 // https://leetcode.com/problems/rotting-oranges/
 public class RottingOranges_994 {
+    // solution1
     int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    public int orangesRotting(int[][] grid) {
+    public int orangesRotting1(int[][] grid) {
         // multi bfs at the same time
         // time O(m * n)
         // space O(m * n)
@@ -66,6 +67,63 @@ public class RottingOranges_994 {
         return res;
     }
 
+    // solution2
+
+    public int orangesRotting2(int[][] grid) {
+        // time O(m * n)
+        // space O(m * n) because of queue
+
+        /**
+         1. add all rotten oranges to queue
+         2. run bfs
+         3. check all grid if fresh exist then return -1
+         */
+        final int ROWS = grid.length;
+        final int COLS = grid[0].length;
+        Queue<int[]> q = new LinkedList<>(); // [r, c, level]
+        boolean[][] visited = new boolean[ROWS][COLS];
+
+        for(int r = 0; r < ROWS; r++)
+            for(int c = 0; c < COLS; c++)
+                if(grid[r][c] == 2)
+                    q.add(new int[]{r,  c, 0});
+
+        int minutes = bfs(grid, q, ROWS, COLS, visited);
+
+        for(int r = 0; r < ROWS; r++)
+            for(int c = 0; c < COLS; c++)
+                if(grid[r][c] == 1 && !visited[r][c])
+                    return -1;
+
+        return minutes;
+    }
+
+    private int bfs(int[][] grid, Queue<int[]> q, int ROWS, int COLS, boolean[][] visited) {
+        int minute = 0;
+        while(!q.isEmpty()) {
+            int[] cell = q.poll();
+            int r = cell[0];
+            int c = cell[1];
+            int level = cell[2];
+            if(visited[r][c])
+                continue;
+
+            visited[r][c] = true;
+            minute = level;
+
+            for(int[] dir : directions) {
+                int newR = dir[0] + r;
+                int newC = dir[1] + c;
+
+                if(newR >= 0 && newR < ROWS && newC >= 0 && newC < COLS && !visited[newR][newC] && grid[newR][newC] == 1) {
+                    q.add(new int[]{newR, newC, level + 1});
+                }
+            }
+        }
+
+        return minute;
+    }
+
     public static void main(String[] args) {
         int[][] arr = new int[][]{
                 {2, 1, 1},
@@ -74,6 +132,6 @@ public class RottingOranges_994 {
         };
 
         RottingOranges_994 oranges_994 = new RottingOranges_994();
-        System.out.println(oranges_994.orangesRotting(arr));
+        System.out.println(oranges_994.orangesRotting2(arr));
     }
 }
