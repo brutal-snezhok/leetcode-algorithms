5324194450eb1b5d38681476e89d2f27a11ec339
DmitryTsyrulik <czyrulik97@mail.ru>
2023-10-12 09:47:39 +0300

    solved WordBreak_139 task

--- src/main/java/dp/WordBreak_139.java
+++ src/main/java/dp/WordBreak_139.java
@@ -1,9 +1,6 @@
 package dp;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 // https://leetcode.com/problems/word-break/
 public class WordBreak_139 {
@@ -99,6 +96,93 @@ public class WordBreak_139 {
         }
 
         return dp[0];
+    }
+
+    // solution5
+    // https://leetcode.com/problems/word-break/solutions/1023786/trie-dfs-solution/
+    public boolean wordBreak5(String s, List<String> wordDict) {
+        // solution: trie + dfs
+        // time O(n^2 + m*k), n - length of s, m - length of wordDict, k - avg length of wordDict
+        // space O(n + m*k)
+
+        Trie trie = new Trie(s.length());
+        for(String word : wordDict)
+            trie.insert(word);
+
+        return trie.dfs(s, 0);
+        // return trie.isWordBreak(s, 0);
+    }
+
+    class Trie {
+
+        Node root;
+        boolean[] visited;
+
+        public Trie(int n) {
+            root = new Node();
+            visited = new boolean[n];
+        }
+
+        public void insert(String word) {
+            Node curr = root;
+            for(char ch : word.toCharArray()) {
+                if(!curr.children.containsKey(ch))
+                    curr.children.put(ch, new Node());
+
+                curr = curr.children.get(ch);
+            }
+
+            curr.isWord = true;
+        }
+
+        // recursive
+         public boolean dfs(String s, int ind) {
+             Node curr = root;
+             if(ind == s.length())
+                 return true;
+
+             if(visited[ind])
+                 return false;
+             visited[ind] = true;
+
+             while(ind < s.length() && curr.children.containsKey(s.charAt(ind))) {
+                 curr = curr.children.get(s.charAt(ind));
+
+                 if(curr.isWord && dfs(s, ind + 1))
+                     return true;
+                 ind++;
+             }
+
+             return false; // not found, backtrack
+         }
+
+         // iterative
+        public boolean isWordBreak(String s, int ind) {
+            for (int i = 0; i < s.length(); i++) {
+                if (i == 0 || visited[i - 1]) {
+                    Node curr = root;
+                    for (int j = i; j < s.length(); j++) {
+                        char c = s.charAt(j);
+                        if (!curr.children.containsKey(c)) {
+                            // No words exist
+                            break;
+                        }
+
+                        curr = curr.children.get(c);
+                        if (curr.isWord) {
+                            visited[j] = true;
+                        }
+                    }
+                }
+            }
+
+            return visited[s.length() - 1];
+        }
+
+        class Node {
+            Map<Character, Node> children = new HashMap<>();
+            boolean isWord;
+        }
     }
 
     public static void main(String[] args) {
